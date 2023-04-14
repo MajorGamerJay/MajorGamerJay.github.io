9958a08289bcc8aec72b6ca4965abab0fc6b1e8c
Oaseu Azad <majorgamerjay@protonmail.com>
2023-04-14 17:47:21 +0600

    finish making book list

--- docs/books.js
+++ docs/books.js
@@ -1,6 +1,7 @@
 /* List of articles */
 var books = document.getElementById('books');
 var plan_to_read = document.getElementById('plan_to_read');
+var authors_select = document.getElementById('author-select');
 
 books.innerHTML = "<tr><th>Name</th><th>Author</th><th>Rating</th></tr>";
 plan_to_read.innerHTML = "<tr><th>Name</th><th>Author</th></tr>";
@@ -28,6 +29,13 @@ function checkTagged(page) {
     return pass;
 }
 
+function checkAuthor(book) {
+    if(book.author == authors_select.options[authors_select.selectedIndex].innerHTML)
+        return true;
+    else
+        return false;
+}
+
 /* Main function to run after filtered tags are applied */
 function reFilter() {
     books.innerHTML = "<tr><th>Name</th><th>Author</th><th>Rating</th></tr>";
@@ -35,25 +43,32 @@ function reFilter() {
     // Fetch data and work with it
     fetch('./books.json').then((response) => response.json())
         .then((json) => {
-            // addFilteredTags(json.tags); // Add filtered tags from all_tags array
+            addFilteredTags(json.tags); // Add filtered tags from all_tags array
             var book_list = [];
+            var plan_to_read_list = [];
+
             book_list = json.books.map(a => ({...a}));
+            plan_to_read_list = json.plan_to_read.map(a => ({...a}));
+
         if (filtered_tags.length != 0)
             book_list = book_list.filter(checkTagged);
+
+        if (!(authors_select.options[authors_select.selectedIndex].innerHTML == "None"))
+            book_list = book_list.filter(checkAuthor);
 
         if (document.getElementById('ascending').checked) {
             book_list.sort((a, b) => {
                 return a.name > b.name;
             });
 
-            plan_to_read_list.sort((a,b) => {
+            plan_to_read_list.sort((a, b) => {
                 return a.name > b.name;
             });
         }
 
         else if (document.getElementById('descending').checked) {
             book_list.sort((a, b) => {
-                return b.name < a.name;
+                return b.name > a.name;
             });
 
             plan_to_read_list.sort((a,b) => {
@@ -63,7 +78,7 @@ function reFilter() {
 
         else if (document.getElementById('top_rate').checked) {
             book_list.sort((a, b) => {
-                return parseFloat(a.rating) > parseFloat(b.rating);
+                return parseFloat(a.rating) < parseFloat(b.rating);
             });
 
             plan_to_read_list.sort((a,b) => {
@@ -73,7 +88,7 @@ function reFilter() {
 
         else if (document.getElementById('least_rate').checked) {
             book_list.sort((a, b) => {
-                return parseFloat(a.rating) < parseFloat(b.rating);
+                return parseFloat(a.rating) > parseFloat(b.rating);
             });
 
             plan_to_read_list.sort((a,b) => {
@@ -88,7 +103,7 @@ function reFilter() {
             });
 
         // Apply changes
-        book_list.forEach(each_page => {
+        book_list.forEach(each_book => {
             books.innerHTML += `<tr><td>${each_book.name}</td><td>${each_book.author}</td><td>${each_book.rating}</td></tr>`;
         });
         plan_to_read_list.forEach(each_book => {
@@ -106,7 +121,10 @@ fetch('./books.json')
     .then((response) => response.json())
     .then((json) => {
         var book_list = json.books.map(a => ({...a})); // Clones the array
-        
+        json.authors.forEach(author => {
+            document.getElementById('author-select').innerHTML += `<option>${author}</option>`;
+        });
+
         book_list.sort((a,b) => {
             return a.name > b.name;
         });
--- src/books.md
+++ src/books.md
@@ -1,12 +1,47 @@
+#### Sort by
+
 <label for="ascending">A-z</label><input id="ascending" name="sort" type="radio" checked="checked">
 <label for="descending">z-A</label><input id="descending" name="sort" type="radio">
-<label for="top-rated">Most liked</label><input id="top-rated" name="sort" type="radio">
-<label for="least-rated">Least liked</label><input id="least-rated" name="sort" type="radio">
+<label for="top_rate">Most liked</label><input id="top_rate" name="sort" type="radio">
+<label for="least_rate">Least liked</label><input id="least_rate" name="sort" type="radio">
+
+#### Filter by
+
+<label for="mystery">Mystery</label><input id="mystery" type="checkbox">
+<label for="thriller">Thriller</label><input id="thriller" type="checkbox">
+<label for="romance">Romance</label><input id="romance" type="checkbox">
+<label for="dystopian">Dystopian</label><input id="dystopian" type="checkbox">
+<label for="non-fiction">Non-Fiction</label><input id="non-fiction" type="checkbox">
+<label for="self-help">Self-Help</label><input id="self-help" type="checkbox">
+<label for="philosophy">Philosophy</label><input id="philosophy" type="checkbox">
+<label for="humayun-ahmed">Humayun Ahmed</label><input id="humayun-ahmed" type="checkbox">
+<label for="himu">Himu</label><input id="himu" type="checkbox">
+<label for="epic">Epic</label><input id="epic" type="checkbox">
+<label for="psychological">Psychological</label><input id="psychological" type="checkbox">
+<label for="drama">Drama</label><input id="drama" type="checkbox">
+<label for="satire">Satire</label><input id="satire" type="checkbox">
+<label for="novel">Novel</label><input id="novel" type="checkbox">
+<label for="short-story">Short Story</label><input id="short-story" type="checkbox">
+<label for="magic-realism">Magic Realism</label><input id="magic-realism" type="checkbox">
+
+<label for="author-select">Author: </label>
+<select id="author-select">
+<option>None</option>
+</select>
 
 <button type="button" onclick="reFilter()">Filter</button>
 
+This list does not contain all the books that I have read in my life, only ones
+that are worth mentioning for discussion if anyone wants. Recently
+I have started recording books I have read and will read, and those that I have
+read after I started recording the list, those are given here.
 
 <table id="books"></table>
+
+Most of the books for this list is added from my college's library, which had a
+lot of old and obscure books that you cannot find anywhere else. So this is more
+of an antiquity list. So, if you are in possession of any of these books and wish
+to sell them to me, I would be glad to buy it. :)
 
 <table id="plan_to_read"><table>
 
